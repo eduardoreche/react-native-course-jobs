7468bc963fdf0bb32fb7fbe57e6f2dd689685aee
Eduardo Reche <edu.reche@gmail.com>
2017-08-07 23:19:34 -0300

    reset liked jobs list

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -5,6 +5,7 @@ import qs from 'qs';
 import {
   FETCH_JOBS,
   LIKE_JOB,
+  CLEAR_LIKED_JOBS,
 } from './types';
 
 const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
@@ -39,4 +40,10 @@ export const likeJob = (job) => {
     payload: job,
     type: LIKE_JOB,
   }
+}
+
+export const clearLikedJobs = () => {
+  return {
+    type: CLEAR_LIKED_JOBS,
+  };
 }
